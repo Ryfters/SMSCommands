562fb1dcdfaa60a99f8b0a78b3bbd05ce65d3433
Ryfter <ryfterwastaken@gmail.com>
2025-07-15 18:38:03 +0100

    Changes

--- app/src/main/java/com/ryfter/smscommands/data/Defaults.kt
+++ app/src/main/java/com/ryfter/smscommands/data/Defaults.kt
@@ -5,7 +5,7 @@ object Defaults {
     const val COLLECT_HISTORY = true
     const val COMMANDS = false
     const val DARK_THEME = 0
-    const val DISMISS_NOTIFICATION_TYPE = 0
+    const val DISMISS_NOTIFICATION_TYPE = 1
     const val DYNAMIC_COLOR = true
     const val IS_FIRST_LAUNCH = true
     const val REQUIRE_PIN = false
