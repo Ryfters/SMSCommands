0e65d29569b2cfabe115d2c720e81cda57d18915
Ryfter <ryfterwastaken@gmail.com>
2025-07-15 14:32:15 +0100

    Changes

--- app/src/main/java/com/ryfter/smscommands/receiver/SMSReciever.kt
+++ app/src/main/java/com/ryfter/smscommands/receiver/SMSReciever.kt
@@ -4,6 +4,8 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.provider.Telephony
+import android.provider.Telephony.Sms.Intents.SMS_RECEIVED_ACTION
+import com.ryfter.smscommands.BuildConfig
 
 class SMSReceiver: BroadcastReceiver() {
     override fun onReceive(context: Context?, intent: Intent?) {
@@ -12,21 +14,27 @@ class SMSReceiver: BroadcastReceiver() {
 
         val sender: String
         val msgBody: String
-        if (intent?.action == "android.provider.Telephony.SMS_RECEIVED") {
-            val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
-            sender = messages[0]?.originatingAddress.toString()
-            msgBody = messages[0].messageBody
-        } else if (intent?.action == ACTION_PROCESS_MESSAGE) {
-            sender = intent.getStringExtra(SENDER_EXTRA) ?: ""
-            msgBody = intent.getStringExtra(MESSAGE_EXTRA) ?: ""
-        } else return
+        when (intent?.action) {
+            SMS_RECEIVED_ACTION -> {
+                val messages = Telephony.Sms.Intents.getMessagesFromIntent(intent)
+                sender = messages[0]?.originatingAddress.toString()
+                msgBody = messages[0].messageBody
+            }
+
+            ACTION_PROCESS_MESSAGE -> {
+                sender = intent.getStringExtra(SENDER_EXTRA) ?: ""
+                msgBody = intent.getStringExtra(MESSAGE_EXTRA) ?: ""
+            }
+
+            else -> return
+        }
 
         processMessage(context, sender, msgBody)
 
     }
 
     companion object {
-        const val ACTION_PROCESS_MESSAGE = "com.ryfter.smscommands.ACTION_PROCESS_MESSAGE"
+        const val ACTION_PROCESS_MESSAGE = "${BuildConfig.APPLICATION_ID}.ACTION_PROCESS_MESSAGE"
         const val SENDER_EXTRA = "sender"
         const val MESSAGE_EXTRA = "message"
     }
